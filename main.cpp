8537342771af818f000c49214b9159e170472fca
StaMeska5 <stameska40@mail.ru>
2024-09-27 23:51:51 +0200

    Add: files for 5 lab 7 variant

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+//1 задание
+    int numbers[] = {1, 2, 3, 4, -7, -14}; //Без понятия как задать последовательность с клавиатуры
+    int sum = 0;
+    int min_37 = 0;
+    int num_min_37;
+
+    for (int i = 0; i < 6; i++)
+        if ((((numbers[i] % 7) == 0) || ((numbers[i] % 3) == 0)) && (numbers[i] < 0))
+        {
+            sum += numbers[i];
+            if (numbers[i] < min_37)
+            {
+                min_37 = numbers[i];
+                num_min_37 = i;
+            }
+        }
+    std::cout << sum << std::endl;
+    std::cout << min_37 << std::endl;
+    std::cout << num_min_37 << std::endl;
+
+//2 задание
+    unsigned int N;
+    int c = 10;
+    int mult = 1;
+
+    std::cin >> N;
+    while (N > 0)
+    {
+        int cifra = N % c;
+        if ((cifra % 2) == 0)
+        {
+            mult *= cifra;
+        }
+        N /= 10;
+    }
+    std::cout << mult << std::endl;
+
+    return 0;
+}
